debe79e248f431c999ff6d158006c27e0c93f506
huwei0612 <huwei.techno@gmail.com>
2026-07-15 00:54:47 +0700

    Delete thuctap directory

--- thuctap/fix_sidebar.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('src/components/Sidebar.tsx', 'utf8');
-
-// The replacement messed up the bottom button and some other variables
-content = content.replace(/onOpenSettings, FileSignatureModal/g, 'onOpenSettingsModal');
-content = content.replace(/<Settings, FileSignature size/g, '<Settings size');
-content = content.replace(/\{\/\* Settings, FileSignature button/g, '{/* Settings button');
-
-fs.writeFileSync('src/components/Sidebar.tsx', content);
